4244d49d2eb9f704aac0bc36e90a2c56aa73ca83
QMoayedpour <quentin.moayedpour@ensae.fr>
2024-01-23 15:30:13 +0000

    Corrrection fonction + appli vrai données

--- utils/create_data.py
+++ utils/create_data.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
-def data_simulation(phi1=0.8, phi2=-0.2, amplitude=1, frequency= 0.03, sigma= 2, n=300):
+def data_simulation(phi1=0.8, phi2=-0.2, trend=0.2,amplitude=1, frequency= 0.03, sigma= 2, n=300, change=True):
 
     amplitude = 1.0
     frequency = 0.03
@@ -10,27 +11,24 @@ def data_simulation(phi1=0.8, phi2=-0.2, amplitude=1, frequency= 0.03, sigma= 2,
 
     y_minus_1 = 0.0
     y_minus_2 = 0.0
-    tren=0.2
+    tren=trend
     y = []
     for t in range(n):
-        trend = tren * t if t < 200 else tren * 200  # Tendance linéaire
+        trend_ = tren * t if t < 200 else tren * 200  # Tendance linéaire
 
         seasonality = amplitude * np.sin(2 * np.pi * frequency * t)
 
         ar2_component = phi1 * y_minus_1 + phi2 * y_minus_2
 
-        if 400 <= t < 500:
-            trend -= 0.1 * (t - 400)
-        if t==499:
-            tren=0.15
+        if 400 <= t < 500 and change:
+            trend_ -= trend/2 * (t - 400)
 
-        y_t = seasonality + ar2_component + white_noise[t]
+        y_t = trend_ + seasonality + ar2_component + white_noise[t]
         y.append(y_t)
 
         y_minus_2 = y_minus_1
         y_minus_1 = y_t
     return np.array(y)
-y = data_simulation(phi1=1.3, phi2=-0.3, sigma=4, n= 10000)
 
 def simple_plot(y):
     plt.plot(y, label='Série simulée')
@@ -39,4 +37,3 @@ def simple_plot(y):
     plt.ylabel('Valeurs')
     plt.legend()
     plt.show()
-simple_plot(y)
